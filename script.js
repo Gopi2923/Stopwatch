009db385faf64ba8f5a79e4861ea6f581c60f908
Gopi2923 <dgopidhikshan@gmail.com>
2024-04-04 19:35:52 +0530

    Format changes

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-let [seconds, minutes, hours] = [0,0,0];
+let [seconds, minutes, hours] = [0, 0, 0];
 let displayTime = document.getElementById('displayTime')
 let timer = null;
 
 function stopwatch() {
     seconds++;
-    if(seconds == 60){
+    if (seconds == 60) {
         seconds = 0;
         minutes++;
-        if(minutes == 60){
+        if (minutes == 60) {
             minutes = 0;
             hours++;
         }
@@ -21,10 +21,10 @@ function stopwatch() {
 }
 
 function watchStart() {
-    if(timer !== null){
+    if (timer !== null) {
         clearInterval(timer);
     }
-   timer = setInterval(stopwatch, 1000);
+    timer = setInterval(stopwatch, 1000);
 }
 
 function watchStop() {
@@ -33,6 +33,6 @@ function watchStop() {
 
 function watchReset() {
     clearInterval(timer);
-    [seconds, minutes, hours] = [0,0,0];
+    [seconds, minutes, hours] = [0, 0, 0];
     displayTime.innerHTML = "00:00:00";
 }
